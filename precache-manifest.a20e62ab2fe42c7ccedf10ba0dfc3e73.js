13c5ea09e59931a8c79fe581bf8a402d635b97e6
gtishenko <gregorytyshchenko3011@gmail.com>
2021-04-25 16:48:43 +0300

    Updates

---
+++ precache-manifest.a20e62ab2fe42c7ccedf10ba0dfc3e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a6cb5eca99b8085e0cc",
+    "url": "/phpform/static/css/main.b2d5be0d.chunk.css"
+  },
+  {
+    "revision": "3a6cb5eca99b8085e0cc",
+    "url": "/phpform/static/js/main.a1aef02c.chunk.js"
+  },
+  {
+    "revision": "b50dd68c221ef1a03074",
+    "url": "/phpform/static/js/runtime~main.9cb2f035.js"
+  },
+  {
+    "revision": "0bc9611835fc9d79f603",
+    "url": "/phpform/static/css/2.8cc3614d.chunk.css"
+  },
+  {
+    "revision": "0bc9611835fc9d79f603",
+    "url": "/phpform/static/js/2.f22090f7.chunk.js"
+  },
+  {
+    "revision": "c6f345aaeaecfa05e592a09f6a79b16a",
+    "url": "/phpform/index.html"
+  }
+];
